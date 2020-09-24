afcd1b3c94ba9690aa190ea7e76274d348763add
Danilsonunes <danilsonunes@gmail.com>
2020-09-24 11:53:22 -0300

    creadas las rutas GET by id, PUT y DELETE

--- controllers/trilhas.controller.js
+++ controllers/trilhas.controller.js
@@ -32,3 +32,37 @@ exports.listAllTrilhas = async (req, res) => {
   const response = await db.query("SELECT * FROM trilhas ORDER BY nome ASC");
   res.status(200).send(response.rows);
 };
+
+// ===> metodo reponsable por listar las trilhas por id
+
+exports.listTrilhasById = async (req, res) => {
+  const trilhaId = parseInt(req.params.id);
+  const response = await db.query(
+    "SELECT * FROM trilhas WHERE trilha_id = $1",
+    [trilhaId]
+  );
+  res.status(200).send(response.rows);
+};
+
+// ===> metodo responsable por actualizar una trilha dependiendo del id (trilha_id)
+
+exports.actualizarTrilhasById = async (req, res) => {
+  const trilhaId = parseInt(req.params.id);
+  const { nome, descripcion } = req.body;
+
+  const response = await db.query(
+    "UPDATE trilhas SET nome = $1, descripcion = $2 WHERE trilha_id = $3",
+    [nome, descripcion, trilhaId]
+  );
+  res.status(200).send({ message: "trilha atualizada satisfatoriamente!" });
+};
+
+// ===> metodo responsable por deletar una trilha dependiendo del id (trilha_id)
+
+exports.deleteTrilhaById = async (req, res) => {
+  const trilhaId = parseInt(req.params.id);
+
+  await db.query("DELETE FROM trilhas WHERE trilha_id = $1", [trilhaId]);
+
+  res.status(200).send({ message: "Trilha deletatada satisfatorimente!" });
+};
--- routes/trilhas.routes.js
+++ routes/trilhas.routes.js
@@ -16,4 +16,13 @@ router.post("/trilhas", trilhasController.createTrilha);
 // ==> ruta responsable por listar todas las trilhas (GET): localhost:3000/api/trilhas
 router.get("/trilhas", trilhasController.listAllTrilhas);
 
+// ==> ruta responsable por listar todas las trilhas by Id (GET) : localhost:3000/api/trilhas:id
+router.get("/trilhas/:id", trilhasController.listTrilhasById);
+
+// ==> ruta responsable por actualizar una trilha dependiendo del Id [trilha_id] (PUT) : localhost:3000/api/trilhas:id
+router.put("/trilhas/:id", trilhasController.actualizarTrilhasById);
+
+// ==> ruta responsable por deletar una trilha dependiendo del Id [trilha_id] (PUT) : localhost:3000/api/trilhas:id
+router.delete("/trilhas/:id", trilhasController.deleteTrilhaById);
+
 module.exports = router;
